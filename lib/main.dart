97ffeede93ebbb0ed75f80d3774109e9dbaafd5e
mini_ <mini_yitan@hotmail.com>
2021-10-29 11:06:54 +0200

    Commit 2: Routes, Alvaro Lodeiro

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        '/pantalla1': (context) => pantalla1()
+      onGenerateRoute:(settings) {
+
+        var uri = Uri.parse(settings.name ??'/');//si es nulo asigna la por defecto
+        if (uri.pathSegments.length == 2 && uri.pathSegments.first == 'navegate') {
+
+          var index = int.parse(uri.pathSegments[1]);
+
+          if(index % 2==0){//pantalla 1
+            return MaterialPageRoute(builder: (context) => pantalla1(id: index,));
+
+          }else{//pantalla2
+            return MaterialPageRoute(builder: (context) => pantalla2(id: index,));
+          }
+
+        }
+        return MaterialPageRoute(builder: (context) => const MyHomePage(title: ''));
+
       },
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -52,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ListTile(
                   title: Text("$index", textAlign: TextAlign.center,style:  TextStyle(fontSize: 30),),
                   onTap: ()=>{
-                    Navigator.pushNamed(context, '/pantalla1',arguments: index)
+                    Navigator.pushNamed(context, '/navegate/$index',arguments: index)
                   },
                 );
             },
@@ -70,33 +85,90 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class pantalla1 extends StatefulWidget {
+  int id;
 
-
+  pantalla1({
+    required this.id,
+  });
   @override
-  _pantalla1State createState() => _pantalla1State();
+  _pantalla1State createState() => _pantalla1State( id: this.id);
 }
 
 class _pantalla1State extends State<pantalla1> {
+  int id;
+  _pantalla1State({
+    required this.id
+  });
+
   @override
   Widget build(BuildContext context) {
-    int number=ModalRoute.of(context)?.settings.arguments as int;
+
     return Scaffold(
       appBar: AppBar(
 
         title: Text("Pantalla 1"),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text("$number", style:  const TextStyle(fontSize: 100),),
-            TextButton(onPressed: () {Navigator.pop(context);},
-              child: const Text("Ir inicio", style:  TextStyle(fontSize: 60),),
-            ),
+      body: Container(
+        color: Colors.orange,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("$id", style:  const TextStyle(fontSize: 100),),
+              TextButton(onPressed: () {Navigator.pop(context);},
+                child: const Text("Ir inicio", style:  TextStyle(fontSize: 60),),
+              ),
 
-          ],
-        ),
+            ],
+          ),
+        ]),
       ),
     );
   }
 }
 
+
+class pantalla2 extends StatefulWidget {
+  int id;
+  pantalla2({
+    required this.id
+  });
+
+  @override
+  _pantalla2State createState() => _pantalla2State(id: this.id);
+}
+
+class _pantalla2State extends State<pantalla2> {
+  int id;
+  _pantalla2State({
+    required this.id
+  });
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("Pantalla 2"),
+      ),
+      body: Container(
+        color: Colors.green,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[ Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text("$id", style:  const TextStyle(fontSize: 100),),
+              TextButton(onPressed: () {Navigator.pop(context);},
+                child: const Text("Ir inicio", style:  TextStyle(fontSize: 60),),
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
